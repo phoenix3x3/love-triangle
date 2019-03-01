708f13752baf4b0c110534cefd88aa1218f40967
phoenix3x3 <fenix3x3@mail.ru>
2019-03-01 14:39:07 +0300

    add solution to task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   let n = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        const condition1 = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+         const condition2 = i + 1 !== preferences[i];
+        if (condition1 && condition2) {
+            n++;
+        }
+    }   
+    return n / 3;
 };
